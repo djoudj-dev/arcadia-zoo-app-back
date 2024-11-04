5920c971c77215c5ef32914cb82ed7be1caea333
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-04 23:06:44 +0100

    Ajout de la documentation et des commentaires dans le module de gestion des comptes utilisateurs : amélioration de la clarté et de la maintenabilité du code pour les contrôleurs, services et modèles.

--- src/modules/admin-dashboard/account-management/models/role.model.ts
+++ src/modules/admin-dashboard/account-management/models/role.model.ts
@@ -1,4 +1,8 @@
+/**
+ * Modèle de données pour un rôle utilisateur.
+ * Représente les informations d'un rôle dans la base de données.
+ */
 export class Role {
-  id: number;
-  name: string;
+  id: number; // Id du rôle
+  name: string; // Nom du rôle
 }
--- src/modules/admin-dashboard/account-management/models/user.model.ts
+++ src/modules/admin-dashboard/account-management/models/user.model.ts
@@ -1,12 +1,16 @@
 import { Role } from './role.model';
 
+/**
+ * Modèle de données pour un utilisateur.
+ * Représente les informations d'un utilisateur dans la base de données.
+ */
 export class User {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-  role: Role;
-  roleId: number;
-  created_at: Date;
-  updated_at: Date;
+  id: number; // Id de l'utilisateur
+  name: string; // Nom de l'utilisateur
+  email: string; // Adresse e-mail de l'utilisateur
+  password: string; // Mot de passe de l'utilisateur
+  role: Role; // Rôle de l'utilisateur
+  roleId: number; // Id du rôle de l'utilisateur
+  created_at: Date; // Date de création de l'utilisateur
+  updated_at: Date; // Date de mise à jour de l'utilisateur
 }
