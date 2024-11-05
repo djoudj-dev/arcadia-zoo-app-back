1333b8c4ea3ef1e4908f301ea43873616366730d
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-05 15:36:43 +0100

    Ajout d'une méthode pour récupérer les animaux d'un habitat par son identifiant dans le contrôleur et le service des habitats.

--- src/modules/habitats/controllers/habitats.controller.ts
+++ src/modules/habitats/controllers/habitats.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Habitat } from 'src/modules/admin-dashboard/habitat-management/models/habitat.model';
 import { HabitatsService } from '../services/habitats.service';
+import { Animal } from 'src/modules/admin-dashboard/animal-management/models/animal.model';
 
 @Controller('/api/habitats')
 export class HabitatsController {
@@ -17,5 +18,9 @@ export class HabitatsController {
   @Get(':id')
   async getHabitatById(@Param('id') id: number): Promise<Habitat> {
     return this.habitatsService.getHabitatById(id);
+  }
+  @Get(':id/animals')
+  async getHabitatAnimals(@Param('id') id: number): Promise<Animal[]> {
+    return this.habitatsService.getHabitatAnimals(id);
   }
 }
