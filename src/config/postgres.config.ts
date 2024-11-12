5455d1f0d35a33d1695650bccdf2b4b65bdc2dda
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-12 12:37:18 +0100

    Add admin user creation function in PostgreSQL config, enhancing setup process with hashed password handling and existence check

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -1,10 +1,11 @@
+import * as bcrypt from 'bcrypt';
 import * as dotenv from 'dotenv';
 import { Pool } from 'pg';
 
 // Charger les variables d'environnement
 dotenv.config();
 
-// Configuration de la connexion à PostgreSQL pour l'environnement local
+// Configuration de la connexion à PostgreSQL pour l'environnement du VPS
 const pool = new Pool({
   host: 'localhost',
   port: 5432,
@@ -18,5 +19,39 @@ export const query = async (text: string, params?: any[]) => {
   return pool.query(text, params);
 };
 
-// Exporter le pool pour l'utiliser dans d'autres fichiers
-export { pool };
+// Fonction pour créer un utilisateur administrateur
+export const createAdminUser = async () => {
+  const adminEmail = 'admin@mail.com';
+
+  // Vérifiez si l'utilisateur admin existe déjà
+  const checkUser = await query('SELECT * FROM users WHERE email = $1', [
+    adminEmail,
+  ]);
+
+  if (checkUser.rows.length > 0) {
+    console.log('L’utilisateur administrateur existe déjà.');
+    return;
+  }
+
+  // Hasher le mot de passe
+  const hashedPassword = await bcrypt.hash('admin123', 10);
+
+  // Insertion de l'utilisateur administrateur
+  const insertUserQuery = `
+    INSERT INTO users (name, email, password, role_id, created_at, updated_at)
+    VALUES ($1, $2, $3, $4, NOW(), NOW())
+  `;
+  await query(insertUserQuery, ['Admin', adminEmail, hashedPassword, 1]);
+
+  console.log('Utilisateur administrateur créé avec succès.');
+};
+
+// Appel de la fonction de création de l'utilisateur administrateur
+createAdminUser()
+  .then(() => console.log('Initialisation terminée'))
+  .catch((err) =>
+    console.error(
+      'Erreur lors de la création de l’utilisateur administrateur :',
+      err,
+    ),
+  );
