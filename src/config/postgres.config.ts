b03f5f921bcac04f7bb81cc589ba9cd5b8a9f159
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-12 13:19:51 +0100

    Refactor PostgreSQL config to use environment variables, improve error handling in `createAdminUser` for robustness during initialization

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -7,11 +7,11 @@ dotenv.config();
 
 // Configuration de la connexion à PostgreSQL pour l'environnement du VPS
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'arcadia', // Utilisateur pour le développement
-  password: 'arcadia78', // Mot de passe pour le développement
-  database: 'arcadia_db', // Nom de la base de données
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.DB_USER || 'arcadia', // Valeur par défaut pour le développement
+  password: process.env.DB_PASSWORD || 'arcadia78', // Valeur par défaut pour le développement
+  database: process.env.DB_NAME || 'arcadia_db', // Nom de la base de données
 });
 
 // Fonction générique pour exécuter une requête
@@ -23,27 +23,34 @@ export const query = async (text: string, params?: any[]) => {
 export const createAdminUser = async () => {
   const adminEmail = 'admin@mail.com';
 
-  // Vérifiez si l'utilisateur admin existe déjà
-  const checkUser = await query('SELECT * FROM users WHERE email = $1', [
-    adminEmail,
-  ]);
+  try {
+    // Vérifiez si l'utilisateur admin existe déjà
+    const checkUser = await query('SELECT * FROM users WHERE email = $1', [
+      adminEmail,
+    ]);
 
-  if (checkUser.rows.length > 0) {
-    console.log('L’utilisateur administrateur existe déjà.');
-    return;
+    if (checkUser.rows.length > 0) {
+      console.log('L’utilisateur administrateur existe déjà.');
+      return;
+    }
+
+    // Hasher le mot de passe
+    const hashedPassword = await bcrypt.hash('admin123', 10);
+
+    // Insertion de l'utilisateur administrateur
+    const insertUserQuery = `
+      INSERT INTO users (name, email, password, role_id, created_at, updated_at)
+      VALUES ($1, $2, $3, $4, NOW(), NOW())
+    `;
+    await query(insertUserQuery, ['Admin', adminEmail, hashedPassword, 1]);
+
+    console.log('Utilisateur administrateur créé avec succès.');
+  } catch (err) {
+    console.error(
+      'Erreur lors de la création de l’utilisateur administrateur :',
+      err,
+    );
   }
-
-  // Hasher le mot de passe
-  const hashedPassword = await bcrypt.hash('admin123', 10);
-
-  // Insertion de l'utilisateur administrateur
-  const insertUserQuery = `
-    INSERT INTO users (name, email, password, role_id, created_at, updated_at)
-    VALUES ($1, $2, $3, $4, NOW(), NOW())
-  `;
-  await query(insertUserQuery, ['Admin', adminEmail, hashedPassword, 1]);
-
-  console.log('Utilisateur administrateur créé avec succès.');
 };
 
 // Appel de la fonction de création de l'utilisateur administrateur
@@ -51,7 +58,7 @@ createAdminUser()
   .then(() => console.log('Initialisation terminée'))
   .catch((err) =>
     console.error(
-      'Erreur lors de la création de l’utilisateur administrateur :',
+      "Erreur lors de l'initialisation de la base de données :",
       err,
     ),
   );
