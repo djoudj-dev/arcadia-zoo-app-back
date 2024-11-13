6b04038669e3f29cab75693701aa6ee451bda1e3
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-13 13:58:23 +0100

    Enhance: update PostgreSQL config to load .env file from project root for improved environment variable handling

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
+import { join } from 'path';
 import { Pool } from 'pg';
 
 // Charger les variables d'environnement
-dotenv.config();
+dotenv.config({ path: join(process.cwd(), '.env') });
 console.log('DB_HOST:', process.env.DB_HOST);
 
 // Configuration de la connexion à PostgreSQL pour l'environnement du VPS
