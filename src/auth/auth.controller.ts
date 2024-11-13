e7c09bf281197f18ac96ec5bcb128ae70dfa5e59
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-13 16:09:19 +0100

    Enhance: specify route prefix for AuthController to improve API clarity and organization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
