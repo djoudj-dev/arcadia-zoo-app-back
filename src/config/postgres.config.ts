c0d40101e48613ddb8da5d7ee7ba9a2df3239c4d
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-13 14:49:48 +0100

    Enhance: set default values for PostgreSQL host and port in config to improve development setup and avoid connection issues

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -8,8 +8,8 @@ console.log('DB_HOST:', process.env.DB_HOST);
 
 // Configuration de la connexion à PostgreSQL pour l'environnement du VPS
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST || 'localhost', // Valeur par défaut pour le développement
+  port: Number(process.env.DB_PORT) || 5432, // Valeur par défaut pour le développement
   user: process.env.DB_USER, // Valeur par défaut pour le développement
   password: process.env.DB_PASSWORD, // Valeur par défaut pour le développement
   database: process.env.DB_NAME, // Nom de la base de données
