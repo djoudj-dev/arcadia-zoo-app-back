d619ce96395fa68e7c87fdade5eecd4fb426407b
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-13 09:48:31 +0100

    Refactor: remove deprecated createAdminUserScript file as admin user logic is handled elsewhere in the codebase

--- src/config/createAdminUserScript.ts
+++
@@ -1,9 +0,0 @@
-// createAdminUserScript.ts
-
-import { createAdminUser } from './postgres.config';
-
-createAdminUser()
-  .then(() => console.log('Utilisateur administrateur créé avec succès.'))
-  .catch((error) =>
-    console.error("Erreur lors de l'ajout de l'utilisateur :", error),
-  );
