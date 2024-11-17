8e9d6eac768bccb501594a11c3eca6306c8c09b8
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-17 11:49:50 +0100

    Refactor config files: update dotenv usage for VPS by removing local env settings. Simplify environment variable loading

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -2,9 +2,9 @@ import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 // Charger les variables d'environnement pour le local
-dotenv.config({ path: './.env.local' });
+//dotenv.config({ path: './.env.local' });
 // Charger les variables d'environnement pour le VPS
-//dotenv.config({ path: './.env' });
+dotenv.config({ path: './.env' });
 
 /**
  * URI de connexion à MongoDB.
