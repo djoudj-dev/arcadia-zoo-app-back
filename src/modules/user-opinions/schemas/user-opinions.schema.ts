958525598aa529ae909cd791c8f6750ad8ceefd9
djoudj-dev <nedellec.julien.78@gmail.com>
2025-01-07 10:32:17 +0100

    Refactor user opinions schema and service to include timestamps for created_at and updated_at. Removed unnecessary console logging in validateUserOpinions method to enhance code clarity and maintainability.

--- src/modules/user-opinions/schemas/user-opinions.schema.ts
+++ src/modules/user-opinions/schemas/user-opinions.schema.ts
@@ -2,22 +2,18 @@ import { Model, Schema } from 'mongoose';
 
 // Définition du schéma pour les avis utilisateurs
 const userOpinionsSchemaDefinition = {
-  // Identifiant unique auto-incrémenté pour chaque avis
   id_opinion: { type: Number, unique: true },
-  // Identifiant de l'utilisateur qui a donné l'avis
   user_id: { type: Number, required: true },
-  // Nom de l'utilisateur qui a donné l'avis
   user_name: { type: String, required: true },
-  // Note donnée par l'utilisateur (entre 1 et 5)
   rating: { type: Number, required: true, min: 1, max: 5 },
-  // Contenu du message/commentaire
   comment: { type: String, required: true },
-  // Statut de l'avis (pending, approved, rejected)
   status: {
     type: String,
     enum: ['pending', 'approved', 'rejected'],
     default: 'pending',
   },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
 };
 
 // Options de configuration du schéma
