6f9b844cfdb56f1e321337141c14afa94d0dc208
djoudj-dev <nedellec.julien.78@gmail.com>
2025-01-11 22:23:51 +0100

    refactor(jwt.strategy): Integrate ConfigService for JWT secret management and remove dotenv dependency

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,18 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import * as dotenv from 'dotenv';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AccountService } from '../../modules/dashboard/admin-dashboard/account-management/services/account.service';
 
-dotenv.config();
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly accountService: AccountService) {
+  constructor(
+    private readonly accountService: AccountService,
+    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+    private readonly configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
